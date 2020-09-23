a8d907d9d84cbc7e2a5682501c8e8b2592a96682
marcellothearcane <maxlreynolds@outlook.com>
2020-09-23 07:13:46 +0100

    Reviewed at https://codereview.stackexchange.com/a/249684/228875

--- index.js
+++ index.js
@@ -14,28 +14,20 @@ module.exports = {
     const htmlFiles = `${buildDir}/**/**.html`
     const excludeFiles = (exclude || []).map((filePath) => `!${filePath.replace(/^!/, '')}`)
     console.info(`Excluding ${excludeFiles.length} ${excludeFiles.length === 1 ? 'file' : 'files'}`)
+
     const lookup = [htmlFiles].concat(excludeFiles)
     const paths = await globby(lookup)
     console.info(`Found ${paths.length} HTML ${paths.length === 1 ? 'file' : 'files'}`)
 
-    const file = paths.reduce((final, path) => {
-      const file = fs.readFileSync(path, 'utf-8')
-      const dom = new JSDOM(file)
+    const processFile = createFileProcessor(buildDir, mergedPolicies, disablePolicies, disableGeneratedPolicies)
 
-      const shouldGenerate = (key) => !(disableGeneratedPolicies || []).includes(key)
-      const scripts = shouldGenerate('scriptSrc') ? generateHashesFromElement(dom, 'script') : []
-      const styles = shouldGenerate('styleSrc') ? generateHashesFromElement(dom, 'style') : []
-      const inlineStyles = shouldGenerate('styleSrc') ? generateHashesFromStyle(dom, '[style]') : []
-  
-      const webPath = path.replace(new RegExp(`^${buildDir}(.*)index\\.html$`), '$1')
-      const cspString = buildCSPArray(mergedPolicies, disablePolicies, {
-        scriptSrc: scripts,
-        styleSrc: [...styles, ...inlineStyles],
-      }).join(' ')
-  
-      final += `${webPath}\n  Content-Security-Policy: ${cspString}\n`
-      return final
-    }, [])
+    const processedFileHeaders = await Promise.all(
+      paths.map(path => fs.promises.readFile(path, 'utf-8').then(processFile(path)))
+    )
+
+    const file = processedFileHeaders
+      .map(({ webPath, cspString }) => `${webPath}\n  Content-Security-Policy: ${cspString}`)
+      .join('\n')
 
     fs.appendFileSync(`${buildDir}/_headers`, file)
 
@@ -68,41 +60,52 @@ function mergeWithDefaultPolicies (policies) {
   return {...defaultPolicies, ...policies}
 }
 
-function generateHashesFromElement (dom, selector) {
-  const hashes = new Set()
-  for (const matchedElement of dom.window.document.querySelectorAll(selector)) {
-    if (matchedElement.innerHTML.length) {
-      const hash = sha256.arrayBuffer(matchedElement.innerHTML)
-      const base64hash = Buffer.from(hash).toString('base64')
-      hashes.add(`'sha256-${base64hash}'`)
+function createFileProcessor (buildDir, mergedPolicies, disablePolicies, disableGeneratedPolicies) {
+  return path => file => {
+    const dom = new JSDOM(file)
+    const shouldGenerate = (key) => !(disableGeneratedPolicies || []).includes(key)
+    const generateHashesFromElement = generateHashes(dom, element => element.innerHTML)
+    const generateHashesFromStyle = generateHashes(dom, element => element.getAttribute('style'))
+
+    const scripts = shouldGenerate('scriptSrc') ? generateHashesFromElement('script') : []
+    const styles = shouldGenerate('styleSrc') ? generateHashesFromElement('style') : []
+    const inlineStyles = shouldGenerate('styleSrc') ? generateHashesFromStyle('[style]') : []
+
+    const webPath = path.replace(new RegExp(`^${buildDir}(.*)index\\.html$`), '$1')
+    const cspString = buildCSPArray(mergedPolicies, disablePolicies, {
+      scriptSrc: scripts,
+      styleSrc: [...styles, ...inlineStyles],
+    }).join(' ')
+
+    return {
+      webPath,
+      cspString,
     }
   }
-  return Array.from(hashes)
 }
 
-function generateHashesFromStyle (dom, selector) {
-  const hashes = new Set()
-  for (const matchedElement of dom.window.document.querySelectorAll(selector)) {
-    if (matchedElement.getAttribute('style').length) {
-      const hash = sha256.arrayBuffer(matchedElement.getAttribute('style'))
-      const base64hash = Buffer.from(hash).toString('base64')
-      hashes.add(`'sha256-${base64hash}'`)
+function generateHashes (dom, getPropertyValue) {
+  return selector => {
+    const hashes = new Set()
+    for (const matchedElement of dom.window.document.querySelectorAll(selector)) {
+      const value = getPropertyValue(matchedElement)
+      if (value.length) {
+        const hash = sha256.arrayBuffer(value)
+        const base64hash = Buffer.from(hash).toString('base64')
+        hashes.add(`'sha256-${base64hash}'`)
+      }
     }
+    return Array.from(hashes)
   }
-  return Array.from(hashes)
 }
 
 function buildCSPArray (allPolicies, disablePolicies, hashes) {
-  const toKebabCase = (string) => string.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase()
+  const camelCaseToKebabCase = (string) => string.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase()
 
-  return Object.keys(allPolicies).reduce((csp, key) => {
-    if ((hashes[key] || allPolicies[key]) && !(disablePolicies || []).includes(key)) {
-      csp.push(
-        hashes[key]
-          ? `${toKebabCase(key)} ${hashes[key].join(' ')} ${allPolicies[key]};`
-          : `${toKebabCase(key)} ${allPolicies[key]};`
-      )
-    }
-    return csp
-  }, [])
+  return Object.entries(allPolicies)
+    .filter(([key, defaultPolicy]) => (hashes[key] || defaultPolicy) && !(disablePolicies || []).includes(key))
+    .map(([key, defaultPolicy]) => {
+      const policy = `${hashes[key] && hashes[key].join(' ') || ''} ${defaultPolicy};`;
+      return `${camelCaseToKebabCase(key)} ${policy};`
+    })
 }
