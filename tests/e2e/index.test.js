f11a9d4668ba3021f0fec349f093dff490286068
marcellothearcane <maxlreynolds@outlook.com>
2020-10-07 16:30:12 +0100

    Log warning if there are more folders than test runs

--- tests/e2e/index.test.js
+++ tests/e2e/index.test.js
@@ -1,10 +1,6 @@
 const fs = require('fs')
 const { onPostBuild } = require('../../index.js')
 
-global.console = {
-  info: () => {},
-}
-
 const folders = [
   ['no-generated-csp', `/\n  Content-Security-Policy: default-src 'self';`],
   ['script-elements', `/\n  Content-Security-Policy: default-src 'self'; script-src 'sha256-QgHoDO3umGbaH6pPry9eZ2aNL1KxNUYO9YINxJhKCFc=' 'sha256-kbu6AoxGeNvnosFj2cT7o23oVjN3kw10MbQJ0oC4opI=';`],
@@ -13,6 +9,14 @@ const folders = [
   ['nested-folders-with-nonindex', `/nested/*\n  Content-Security-Policy: default-src 'self';\n/\n  Content-Security-Policy: default-src 'self';`],
   ['scripts-and-styles', `/\n  Content-Security-Policy: default-src 'self'; script-src 'sha256-HgP/C/HfzlJIctOVi/okNAgn76d6kykP7D0jrm1nVu0='; style-src 'sha256-ZBTj5RHLnrF+IxdRZM2RuLfjTJQXNSi7fLQHr09onfY=' 'sha256-0brZ5JHU3lwo7f/rLUc9Nu1kRemuKlxjcMBYIpWAFe0=';`],
 ]
+
+if (folders.length < fs.readdirSync('./tests/e2e/', { withFileTypes: true }).filter(dirent => dirent.isDirectory()).length) {
+  console.warn('\u001b[37m\u001b[43m\u001b[1m WARN \u001b[22m\u001b[49m\u001b[39m Some folders are missing from test run')
+}
+
+global.console = {
+  info: () => {},
+}
 
 test.each(folders)('%s', async (folder, expected) => {
   const inputs = {
